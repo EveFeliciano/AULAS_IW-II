e659ea5526f8329ffa82f53b8c9532cd9d02033d
EveFeliciano <evellynsanfeliciano@gmail.com>
2024-02-25 21:37:06 -0300

    lampada

--- ExercicioLampada/JS/script.js
+++ ExercicioLampada/JS/script.js
@@ -2,6 +2,7 @@ const ligar = document.getElementById('ligar')
 const desligar = document.getElementById('desligar')
 const resetar = document.getElementById('resetar')
 const lampada = document.getElementById('lamp')
+const estado = document.getElementById('estado');
 
 function estaQuebrada(){
     return lampada.src.indexOf('quebrada') > -1
@@ -10,15 +11,18 @@ function estaQuebrada(){
 function lampLigada(){
     if (!estaQuebrada()){
         lampada.src = "img/ligada.jpg"
+        estado.textContent = "a lâmpada está ligada"
     }
 }
 function lampDesligada(){
     if (!estaQuebrada()){
         lampada.src = "img/desligada.jpg"
+        estado.textContent = "a lâmpada está desligada"
     }
 }
 function lampQuebrada(){
     lampada.src = "img/quebrada.jpg"
+    estado.textContent = "a lâmpada está quebrada, resete"
 }
 function lampResetar(){
         lampada.src = "img/desligada.jpg"
