7dfa457efaf083fccfa20e98f76734cff5694dea
Feliciano <124791727+EveFeliciano@users.noreply.github.com>
2024-03-17 00:30:26 -0300

    Add files via upload

--- AULA 5/ex2/js/script2.js
+++ AULA 5/ex2/js/script2.js
@@ -1,6 +1,6 @@
 function Evento(){
     var nome = prompt('Qual o seu nome?')
     console.log(typeof(nome))
-    legenda.innerHTML = 'Olá! Prazer em te , ' + nome
+    legenda.innerHTML = 'Olá! Prazer em te conhecer, ' + nome
 }
 
